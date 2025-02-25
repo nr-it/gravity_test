59a67498f7126365944ee2a6c1f47a34aa81d953
nr-it <141993633+nr-it@users.noreply.github.com>
2025-02-25 14:07:16 +0530

    Update dsa_len_seq.js

--- dsa_len_seq.js
+++ dsa_len_seq.js
@@ -1,12 +1,12 @@
 function lengthOfLIS(nums) {
     if (nums.length === 0) return 0;
 
-    let dp = []; // Stores the increasing subsequence
+    let dp = [];
 
     for (let num of nums) {
         let left = 0, right = dp.length;
 
-        // Binary search for the correct position to replace or insert
+    
         while (left < right) {
             let mid = Math.floor((left + right) / 2);
             if (dp[mid] < num) {
@@ -16,7 +16,6 @@ function lengthOfLIS(nums) {
             }
         }
 
-        // Replace or add
         if (left < dp.length) {
             dp[left] = num;
         } else {
@@ -27,5 +26,4 @@ function lengthOfLIS(nums) {
     return dp.length;
 }
 
-// Example usage
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])); // Output: 4
